d3371a6b3e7ff22a77bf3130d31c792c2f0c04b4
szamo14 <czernecki.michal@gmail.com>
2019-02-12 21:35:29 +0100

    EventController testing

--- src/Evento.API/Controllers/EventsController.cs
+++ src/Evento.API/Controllers/EventsController.cs
@@ -1,7 +1,22 @@
+using System.Threading.Tasks;
+using Evento.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Evento.API.Controllers
 {
-    public class EventsController
+    [Route("[controller]")]
+    public class EventsController : Controller
     {
-        
+        private readonly IEventService _eventService ;
+        public EventsController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(string name)
+        {
+            var events = await _eventService.BrowseAsync(name);
+            return Json(events);
+        }
     }
 }
