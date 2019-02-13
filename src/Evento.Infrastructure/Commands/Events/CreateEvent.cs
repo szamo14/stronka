ff23fd078cd9cef5e14d4cc79364f47cfe73ea31
szamo14 <czernecki.michal@gmail.com>
2019-02-13 08:55:30 +0100

    Dzialajcy

--- src/Evento.Infrastructure/Commands/Events/CreateEvent.cs
+++ src/Evento.Infrastructure/Commands/Events/CreateEvent.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace Evento.Infrastructure.Commands.Events
 {
     public class CreateEvent
     {
-        
+        public Guid EventId { get; set;}
+        public string Name { get;  set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get;  set; }
+        public DateTime EndDate { get;  set; }
     }
 }
