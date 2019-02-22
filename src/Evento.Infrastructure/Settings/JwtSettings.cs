9bcbcda6a8a718252a5bbcb661f460530e3abe54
szamo14 <czernecki.michal@gmail.com>
2019-02-22 12:30:07 +0100

    restore

--- src/Evento.Infrastructure/Settings/JwtSettings.cs
+++ src/Evento.Infrastructure/Settings/JwtSettings.cs
@@ -3,7 +3,7 @@ namespace Evento.Infrastructure.Settings
     public  class JwtSettings
     {
         public string Secret { get ; set; }
-        public string Issuer { get; set; }
+        //public string Issuer { get; set; }
         public int ExpiryMinutes { get; set; }
     }
 }
